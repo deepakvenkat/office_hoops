b418ec8ddd6bd9eec4b8795cd1d56fdd5c3b9873
deepakvenkat <dipak2911@gmail.com>
2013-12-25 14:05:16 -0500

    create player action and style the game page

--- public/js/views/game.js
+++ public/js/views/game.js
@@ -3,7 +3,57 @@ app.GameView = Backbone.View.extend({
   initialize : function () {},
 
   render : function () {
+    this.currentRound = 1;
+    this.players = [{
+      username: "testplayer1"
+    },
+    {
+      username : "testplayer2"
+    },
+    {
+      username : "testplayer3"
+    }]
+
+    this.game = {
+      players : {
+        "testplayer1" : {
+          score : 0,
+          next : "testplayer2"
+        },
+
+        "testplayer2" : {
+          score : 0,
+          next : "testplayer3"
+        },
+
+        "testplayer3" : {
+          score : 0 ,
+        },
+      }
+    }
     this.$el.html(this.template);
+
+    for (var i = 0; i < this.players.length; i++) {
+      var player = this.players[i];
+      var playerView = new app.PlayerView();
+      playerView.render({username: player.username});
+
+      $("#accordion", this.$el).append(playerView.$el);
+    }
+    $("#" + this.players[0].username, this.$el).addClass("in");
     return this;
+  },
+
+  playerAction : function (player, action) {
+    $("#" + player, this.el).collapse("toggle");
+    if (action === "hit") this.game.players[player].score += 1;
+
+    var nextPlayer = this.game.players[player].next;
+    if (!nextPlayer) {
+      this.currentRound++;
+      $(".round", this.el).text(this.currentRound);
+      var nextPlayer = this.players[0].username;
+    }
+    $("#" + nextPlayer, this.el).collapse("toggle");
   }
 });
--- public/js/views/player.js
+++ public/js/views/player.js
@@ -2,8 +2,27 @@ app.PlayerView = Backbone.View.extend({
 
   initialize : function () {},
 
-  render : function () {
-    this.$el.html(this.template);
+  className : "panel panel-default",
+
+  events: {
+    "click .hit" : "playerHit",
+    "click .miss" : "playerMiss"
+  },
+
+  render : function (params) {
+    this.player = params.username;
+    this.currentScore = 0;
+    this.$el.html(this.template({player: params.username}));
     return this;
+  },
+
+  playerHit : function (){
+    this.currentScore++;
+   $(".current-score", this.el).text(this.currentScore);
+    app.gameView.playerAction(this.player, "hit");
+  },
+
+  playerMiss : function () {
+    app.gameView.playerAction(this.player, "miss");
   }
 })
